5fd0a29a3733de715294362feefae0840d6172b9
chixpix1488 <nikitashmyrov2407@gmail.com>
2024-04-21 13:22:58 +0300

    clicker full

---
+++ script.js
@@ -0,0 +1,22 @@
+let cookie = document.querySelector(".cookie")
+let scoreHTML = document.querySelector(".score")
+let cookie2 = document.querySelector(".cookieIMG")
+let score = 0
+
+cookie.addEventListener("click", function(){
+    score = score + 1
+    scoreHTML.innerHTML = score
+
+
+})
+
+
+
+
+
+cookie2.addEventListener("click", function(){
+    score = score + 1
+    scoreHTML.innerHTML = score
+})
+
+
